0a75c0c11f27aafd691178eefa1ce758cbbe6985
Renan <renan.nan.amaral@hotmail.com>
2025-08-02 23:50:14 -0300

    novo poolclientid

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_lVvewBKoF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2e2cv949sd6058vihbmbfvgkso', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '227d6vucppihote3u94g6qo2gr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
